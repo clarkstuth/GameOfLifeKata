7d1498c30ebcf0fd417aad544f437ad8cbd02097
Clark Stuth <clarkstuth@gmail.com>
2014-06-10 22:30:36 -0500

    trying to figure out git vs plugin...

---
+++ GameOfLifeKata/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeKata
+{
+    public class Game
+    {
+        public Cell[,] GameBoard { get; private set; }
+
+        public Game(int boardWidth, int boardHeight)
+        {
+            GameBoard = new Cell[boardWidth, boardHeight];
+
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            for (var x = 0; x < GameBoard.Length/GameBoard.GetLength(1); x++)
+            {
+                for (var y = 0; y < GameBoard.GetLength(1); y++)
+                {
+                    GameBoard[x, y] = new Cell();
+                }
+            }
+        }
+
+        private void StartRandomCell(Random random)
+        {
+            int col, row;
+            do
+            {
+                col = random.Next(GameBoard.Length / GameBoard.GetLength(1));
+                row = random.Next(GameBoard.GetLength(1));
+            } while (GameBoard[col,row].Alive);
+            GameBoard[col,row].Alive = true;
+        }
+
+        public void StartGame(int numberOfAliveCells)
+        {
+            var aliveCells = 0;
+            while (aliveCells < numberOfAliveCells)
+            {
+                var random = new Random();
+                StartRandomCell(random);
+                ++aliveCells;
+            }
+        }
+    }
+}
---
+++ GameOfLifeKataTests/GameBoardTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using GameOfLifeKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeKataTests
+{
+    [TestClass]
+    public class GameBoardTest
+    {
+        [TestMethod]
+        public void GameShouldCreateGameBoardWithHeight2ShouldReturnGameBoardOfHeight2()
+        {
+            var height = 2;
+            var width = 3;
+            var game = InitializeGame(width, height);
+
+            Assert.AreEqual(height, game.GameBoard.Rank);
+        }
+
+        private Game InitializeGame(int width, int height)
+        {
+            var game = new Game(width, height);
+            return game;
+        }
+
+        [TestMethod]
+        public void GameShouldCreateGameBoardWithWidth3ShouldReturnGameBoardOfWidth3()
+        {
+            var height = 2;
+            var width = 3;
+            var game = InitializeGame(width, height);
+
+            Assert.AreEqual(width, game.GameBoard.Length/game.GameBoard.Rank);
+        }
+
+        [TestMethod]
+        public void GameBoardShouldBePopulatedWithCells()
+        {
+            var height = 2;
+            var width = 3;
+            var game = InitializeGame(width, height);
+
+            Assert.IsInstanceOfType(game.GameBoard[0,0], typeof(Cell));
+        }
+
+        [TestMethod]
+        public void CellsShouldStartDead()
+        {
+            var height = 2;
+            var width = 3;
+            var game = InitializeGame(width, height);
+
+            foreach (var cell in game.GameBoard)
+            {
+                Assert.IsFalse(cell.Alive);
+            }
+        }
+
+        [TestMethod]
+        public void StartingTheGameShouldMakeTheSpecifiedNumberOfCellsTurnAlive()
+        {
+            int numCells = 2;
+            var height = 2;
+            var width = 3;
+            var game = InitializeGame(width, height);
+            game.StartGame(numCells);
+
+            int cellCount = 0;
+            foreach (var cell in game.GameBoard)
+            {
+                if (cell.Alive) cellCount++;
+                Console.WriteLine("test");
+            }
+            Assert.AreEqual(numCells, cellCount);
+        }
+
+        [TestMethod]
+        public void GameStartsShouldBeRandom()
+        {
+            var width = 20;
+            var height = 40;
+            var numCells = 3;
+
+            var game = InitializeGame(width, height);
+            game.StartGame(numCells);
+
+            var game2 = InitializeGame(width, height);
+            game2.StartGame(numCells);
+
+            Assert.AreNotEqual(game.GameBoard, game2.GameBoard);
+        }
+
+
+
+    }
+}
